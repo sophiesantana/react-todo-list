d04bf4646eb4dcf4b87c4540b31fc1eb9bf43aa6
Sophie Santana <ssophiesantana@gmail.com>
2024-08-07 13:26:34 -0300

    feat: implementa services para conexao com o banco

--- src/services/todoListService.ts
+++ src/services/todoListService.ts
@@ -1,23 +1,29 @@
 import { Tasks } from "../interfaces/todoListInterface";
 import api from "./api";
 
-
 export const getTasks = (): Promise<Tasks[]> => {
   return api.get<Tasks[]>('/todo-list')
-    .then((response) => response.data)
-    .catch((error) => {
-      console.error('Erro ao buscar tasks:', error);
-      return [];
-    });
+    .then((response) => response.data);
 }
 
-export const createTask = (task: Tasks): Promise<void> => {
+export const createTask = (name: string): Promise<void> => {
+  const task = { name };
   return api.post('/todo-list', task)
     .then(() => {
       console.log('Task registrada com sucesso!');
     })
     .catch((error) => {
       console.error('Erro ao registrar task:', error);
+    });
+}
+
+export const updateTaskById = (task: Tasks) => {
+  return api.patch(`/todo-list/${task.id}`, { done: !task.done })
+    .then(() => {
+      console.log('Task atualizada com sucesso!');
+    })
+    .catch((error) => {
+      console.error('Erro ao atualizar task:', error);
     });
 }
 
