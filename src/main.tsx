ffa8250e13c08bf91228a4dd7a13780c4bdc32e4
Sophie Santana <ssophiesantana@gmail.com>
2024-08-07 13:37:01 -0300

    feat: envolve app no QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './global.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 2 // 2h
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
